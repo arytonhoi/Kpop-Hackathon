db2ac225121be33c27888eefbb74e853925b7b58
Aryton Hoi <aryton.a.hoi@gmail.com>
2018-03-24 20:40:00 -0400

    added lyric analyzer script

---
+++ lyricAnalyzer.py
@@ -0,0 +1,42 @@
+'''
+Given a set of lyrics, analyzes:
+- # English words
+- Total words
+- obscene words
+- #lines
+- Average line length
+- % unique words
+- # Title occurrences in lyrics
+'''
+
+def countEnglishWords(...){
+    ...
+}
+
+def countTotalWords(){
+
+}
+
+def countObsceneWords(){
+
+}
+
+def countLines(){
+
+}
+
+def findAvgLineLength(){
+
+}
+
+def uniqueWordsPercentage(){
+
+}
+
+def countTitleOccurs(){
+
+}
+
+def main(){
+
+}
